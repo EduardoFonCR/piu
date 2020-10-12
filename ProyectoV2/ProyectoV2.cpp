02c650772b40e7e5325431fbeb53360cc44adc7d
Eduardo Fonseca <edufon00@outlook.com>
2020-10-12 15:46:05 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoV2/ProyectoV2.cpp
@@ -0,0 +1,9 @@
+// ProyectoV2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
